6e94945113af3a04287730b707c9c1c87e8f7b35
Muhammad Hijir Sajad <sajadhijir@gmail.com>
2022-11-25 14:55:55 +0800

    fix bug about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import CardAbout from "./Card/CardAbout";
 
 const About = () => {
   return (
-    <div className="lg:grid grid-cols-5 px-5 absolute -top-16 bg-white py-5 rounded-3xl z-10 shadow-xl hidden space-x-2">
+    <div className="lg:grid grid-cols-5 px-5 absolute -top-16 bg-white py-5 rounded-3xl z-10 shadow-xl hidden space-x-2 w-full">
       <div className="">
         <CardAbout gambar={"https://www.ruangguru.com/hubfs/Ruangguru%20Revamp%202022/Product%20Selection/Icon%20Video%20Belajar.svg"} ket={"Video Belajar"} ket1={"Pelajar dapat mengakses seluruh video yang telah direkam"} />
       </div>
